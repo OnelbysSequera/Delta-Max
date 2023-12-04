1de574bbd7b6200df0939c6c4b1724e7072697eb
Manuel <manuelsangroni@gmail.com>
2023-12-04 14:19:01 -0400

    first commit

---
+++ js/carousel.js
@@ -0,0 +1,31 @@
+const carouselSlide = document.querySelector('.carousel-slide');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+
+let counter = 0;
+const slideWidth = carouselSlide.clientWidth;
+
+nextBtn.addEventListener('click', () => {
+  if (counter < carouselSlide.children.length - 1) {
+    counter++;
+    carouselSlide.style.transform = `translateX(-${counter * slideWidth}px)`;
+  }
+});
+
+prevBtn.addEventListener('click', () => {
+  if (counter > 0) {
+    counter--;
+    carouselSlide.style.transform = `translateX(-${counter * slideWidth}px)`;
+  }
+});
+
+function autoSlide() {
+  if (counter < carouselSlide.children.length - 1) {
+    counter++;
+  } else {
+    counter = 0;
+  }
+  carouselSlide.style.transform = `translateX(-${counter * slideWidth}px)`;
+}
+
+setInterval(autoSlide, 3000);
