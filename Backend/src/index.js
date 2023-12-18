1e2011374c03544e13eae7b824f8051975939c76
OnelbysSequera <onelbisalejandrosequera@gmail.com>
2023-12-18 01:14:56 +0100

    detalles

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,24 +1,41 @@
 const express = require ('express');
 const morgan = require ('morgan');
 const exphbs = require ('express-handlebars');
+const path = require ('path');
 
 // Initialization
 const app = express ();
 
 // Settings
 app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs.engine({
+    defaultlayout: 'main',
+    layoutsdir: path.join(app.get('views'), 'layouts'),
+    partialsdir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    helpers: require('./lib/handlebars')
+}));
+
+app.set('views engine', '.hbs');
 
 // Middlewares
 app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 // Global variables
-
+app.use((req, res, next) => {
+    next();
+});
 
 // Routes
 app.use(require('./routes'));
+app.use(require('./routes/authentication'));
+app.use('/links', require('./routes/links'));
 
 // Public
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Starting the server
 app.listen(app.get('port'), () => {
