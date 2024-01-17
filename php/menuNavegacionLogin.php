5a61496cb5b4c1461209b0055e61d27cc441ffac
Julioparadas7 <151449414+Julioparadas7@users.noreply.github.com>
2024-01-17 19:41:17 -0400

    Add files via upload
    Agregando una carpeta php con los menu de navegacion del login y del register

---
+++ php/menuNavegacionLogin.php
@@ -0,0 +1,6 @@
+<nav class="navegacion">
+    <a class="navegacion__enlaces" href="../index.html">Inicio</a>
+    <a class="navegacion__enlaces" href="#">Ofertas</a>
+    <a class="navegacion__enlaces" href="../nosotros.html">Nosotros</a>
+    <a class="navegacion__enlaces" href="register.php">Registrarse</a>
+</nav>
---
+++ php/menuNavegacionRegister.php
@@ -0,0 +1,6 @@
+<nav class="navegacion">
+    <a href="../index.html">Inicio</a>
+    <a href="#">Ofertas</a>
+    <a href="../nosotros.html">Nosotros</a>
+    <a href="login.php">Iniciar Sesion</a>
+</nav>
