169f4a4bc29e3b8daf425a170bf658631fd6ecc0
Torres15k <137131671+Torres15k@users.noreply.github.com>
2024-01-17 22:21:44 -0400

    Create Archivo para establecer la conexion.php

---
+++ Login - Register/php/conexion.php
@@ -0,0 +1,13 @@
+<?php
+/* Establecer la conexión con la base de datos*/
+
+$conexion = new mysqli("localhost", "root", "", "minegocio");
+
+	/*if ($conexion) {
+		echo "Se ha establecido la conexion con la base de datos exitosamente";
+	} else {
+		echo "No se ha podido establacer la conexion con la base de datos";
+	}*/
+
+
+?>
