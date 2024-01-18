4749a031ac0e7c928cbbe57104937d7f94f7c6f4
Julioparadas7 <151449414+Julioparadas7@users.noreply.github.com>
2024-01-17 20:13:15 -0400

    Add files via upload
    agregando el menu de navegacion de laa tienda en una carpeta php

---
+++ php/menuNavegacionTienda.php
@@ -0,0 +1,7 @@
+<nav class="navegacion">
+    <a class="navegacion__enlaces navegacion__enlaces--activo" href="index.php">Inicio</a>
+    <a class="navegacion__enlaces" href="">Ofertas</a>
+    <a class="navegacion__enlaces" href="nosotros.php">Nosotros</a>
+    <a class="navegacion__enlaces" href="Login - Register/login.php">Iniciar Sesion</a>
+    <a class="navegacion__enlaces" href="Login - Register/register.php">Registrarse</a>
+</nav>
